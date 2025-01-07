0ba7d02ee04d072820f73ee4e88cfdceccf74d30
mehediFluttere <mehedii.edu.bd@gmail.com>
2025-01-08 02:54:50 +0600

    scan id screen

---
+++ lib/screen/auth/registration_screen/scan_id/scan_id_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScanIdScreen extends StatefulWidget {
+  const ScanIdScreen({super.key});
+
+  @override
+  State<ScanIdScreen> createState() => _ScanIdScreenState();
+}
+
+class _ScanIdScreenState extends State<ScanIdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
