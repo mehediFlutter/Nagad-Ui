7e35826b593367250fe44274ca86c029eab97241
mehediFluttere <mehedii.edu.bd@gmail.com>
2025-01-19 17:33:49 +0600

    visa

--- lib/screen/transfer_money_screen/transfer_money_screen.dart
+++ lib/screen/transfer_money_screen/transfer_money_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nagad_ui/screen/re_usable_base_screen.dart';
+import 'package:nagad_ui/screen/widgets/app_bar/custom_app_bar.dart';
 
 class TransferMoneyScreen extends StatefulWidget {
   const TransferMoneyScreen({super.key});
@@ -13,7 +14,25 @@ class _TransferMoneyScreenState extends State<TransferMoneyScreen> {
   Widget build(BuildContext context) {
     return ReUsableBaseScreen(child: Column(
       children: [
-        Text("Transfer Money Screen")
+        CustomAppBar(title: 'Transfer Money'),
+       Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 12),
+         child: Column(
+           children: [
+             Material(
+              borderRadius: BorderRadius.circular(12),
+              elevation: 4,
+               child: Container(
+                height: 80,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12)
+                ),
+               ),
+             ),
+           ],
+         ),
+       )
       ],
     ));
   }
